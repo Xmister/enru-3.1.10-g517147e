11b1437f45dc89e25aacb789f6d294d318074972
Xmister <xmisterhu@gmail.com>
2014-01-28 13:53:14 +0100

    Update USB driver
    Signed-off-by: Xmister <xmisterhu@gmail.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1337,9 +1337,15 @@ static int mass_storage_function_init(struct android_usb_function *f,
 		if (config->fsg.nluns > FSG_MAX_LUNS)
 			config->fsg.nluns = FSG_MAX_LUNS;
 		for (i = 0; i < config->fsg.nluns; i++) {
+			if (dev->pdata->cdrom_lun & (1 << i)) {
+				config->fsg.luns[i].cdrom = 1;
+				config->fsg.luns[i].removable = 1;
+				config->fsg.luns[i].ro = 1;
+			} else {
 				config->fsg.luns[i].cdrom = 0;
 				config->fsg.luns[i].removable = 1;
 				config->fsg.luns[i].ro = 0;
+			}
 		}
 	} else {
 		/* default value */
